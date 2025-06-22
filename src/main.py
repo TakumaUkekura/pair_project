08d52521a354afb856e93257dbc9d04683fbe3c9
ukekura <ukekura.takuma@gmail.com>
2025-06-22 11:03:53 +0900

    for処理の縦割り

--- src/main.py
+++ src/main.py
@@ -19,41 +19,55 @@ def input():
 
 
 def create_invoice_content(invoice, plays):
+
     invoice_title = "請求書"
     company_name = "会社名：" + invoice[0]["customer"]
-    trade_contents = ""
-    total_price = 0
-    total_point = 0
+    trade_content = ""
 
-    seikyuusyo = "請求書" + "\n"
-    seikyuusyo += "会社名：" + invoice[0]["customer"] + "\n"
-    goukei = 0
-    point = 0
+    trade_content += invoice_title + "\n"
+    trade_content += company_name + "\n"
 
+    performance_name_list = get_performances_name_list(invoice, plays)
+    audience_number_list = get_audience_number_list(invoice)
+    price_list = get_price_list(invoice, plays)
+
+    for i in range(len(invoice[0]["performances"])):
+        trade_content += "・" + performance_name_list[i] + "（観客数："
+        trade_content += str(audience_number_list[i]) + "人、金額："
+        trade_content += str(price_list[i]) + "）\n"
+    
+    invoice_content = trade_content
+
+    return invoice_content
+
+
+def get_performances_name_list(invoice, plays):
+    performance_name_list = []
     for performance in invoice[0]["performances"]:
+        performance_name_list.append(plays[performance["playID"]]["name"])
+    return performance_name_list
 
-        point += 1 * (performance["audience"] - 30)
+def get_audience_number_list(invoice):
+    audience_number_list = []
+    for performance in invoice[0]["performances"]:
+        audience_number_list.append(performance["audience"])
+    return audience_number_list
 
+def get_price_list(invoice, plays):
+    price_list = []
+    for performance in invoice[0]["performances"]:
         if plays[performance["playID"]]["type"] == "tragedy":
             Ryoukin = 40000 # 基本料金40000$
             if (performance["audience"] > 30):# 観客数が30人を超過する場合
                 Ryoukin += (1000 * (performance["audience"] - 30)) # 超過一人当たり1000$
         if plays[performance["playID"]]["type"] == "comedy":
-            point += 1 * performance["audience"]//5
             Ryoukin = 30000 # 基本料金30000$
             if (performance["audience"] > 20): # 観客数が20人を超える場合、
                 Ryoukin += 1000 # 10000$を追加した上で、
                 Ryoukin += (500 * (performance["audience"] - 20)) # さらに超過一人当たり500$
+        price_list.append(Ryoukin)
+    return price_list
 
-        goukei += Ryoukin
-
-        seikyuusyo += "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、" + "金額：" + str(Ryoukin) + "円）\n"
-
-    seikyuusyo += "合計金額：" + str(goukei) + "円\n"
-    seikyuusyo += "獲得ポイント：" + str(point) + "pt"
-
-    invoice_content = seikyuusyo
-    return invoice_content
 
 def output(output):
     print(output)
